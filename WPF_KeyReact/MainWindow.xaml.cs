01ae485903d13fe7041aa22010316eaea85e7a20
vojtechlengal <vojta.lengal@gmail.com>
2018-02-11 19:48:57 +0100

    Add project files.

---
+++ WPF_KeyReact/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_KeyReact
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Transformer transformer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
+            transformer = new Transformer();
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            Thickness margin = ButtonCar.Margin;
+
+            switch (e.Key)
+            {
+                case Key.Up:
+                    Tuple<double, double> margins = transformer.CountMargin();
+                    margin.Top += margins.Item1;
+                    margin.Left += margins.Item2;
+                    break;
+                case Key.Left:
+                    transformer.Angle -= Transformer.rotationAngle;
+                    break;
+                case Key.Down:
+                    break;
+                case Key.Right:
+                    transformer.Angle += Transformer.rotationAngle;
+                    break;
+                default:
+                    return;
+            }
+
+            ButtonCar.Margin = margin;
+            ButtonCar.RenderTransform = new RotateTransform(transformer.Angle);
+        }
+    }
+}
---
+++ WPF_KeyReact/Transformer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_KeyReact
+{
+    class Transformer
+    {
+        public static readonly int rotationAngle = 30;
+
+        /// <summary>
+        /// úhel
+        /// </summary>
+        private int angle;
+        public int Angle
+        {
+            get
+            {
+                return angle;
+            }
+            set
+            {
+                int newAngle = value % 360;
+                angle = newAngle < 0 ? newAngle + 360 : newAngle;
+            }
+        }
+
+        /// <summary>
+        /// konstruktor
+        /// </summary>
+        public Transformer()
+        {
+            angle = 0;
+        }
+
+        /// <summary>
+        /// spočítá margin
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <returns></returns>
+        public Tuple<double, double> CountMargin()
+        {
+            int topBottom = -1;
+            int leftRight = -1;
+
+            if (angle <= 90)
+            {
+            }
+            else if (angle <= 180)
+                leftRight = 1;
+            else if (angle <= 270)
+            {
+                topBottom = 1;
+                leftRight = 1;
+            }
+            else
+                topBottom = 1;
+
+            double myAngle = angle * (Math.PI / 180);
+            double topMargin = topBottom * Math.Abs(Math.Sin(myAngle));
+            double leftMargin = leftRight * Math.Abs(Math.Cos(myAngle));
+
+            return new Tuple<double, double>(topMargin, leftMargin);
+        }
+    }
+}
