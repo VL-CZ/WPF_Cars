46e0c03231587763f8fccab696f7dde090d988ee
vojtechlengal <vojta.lengal@gmail.com>
2018-03-01 20:56:47 +0100

    smal changes

--- WPF_KeyReact/App.xaml.cs
+++ WPF_KeyReact/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace WPF_KeyReact
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for App.xaml 
     /// </summary>
     public partial class App : Application
     {
